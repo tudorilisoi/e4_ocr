1f391bb44b32f19b9e4604932d906475c34dc704
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-06-02 03:26:39 +0300

    WIP; crop area without action
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,205 @@
-import 'dart:async';
-import 'dart:math';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:photo_manager/photo_manager.dart';
-import 'package:zoomable_image_cropper/zoomable_image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Zoomable cropper Demo',
+      title: 'Image Cropper',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.teal,
       ),
-      home: const MyHomePage(),
+      home: const ImageSelectorScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
+class ImageSelectorScreen extends StatefulWidget {
+  const ImageSelectorScreen({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ImageSelectorScreen> createState() => _ImageSelectorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  ZoomableImageCropperController? controller;
+class _ImageSelectorScreenState extends State<ImageSelectorScreen> {
+  File? _image;
 
-  final boxHeight = 500.0;
-
-  bool loadingImage = true;
-  @override
-  void initState() {
-    getPermissionAndSelectImage().then((value) {
-      Size size = MediaQuery.of(context).size;
-      setState(() {
-        controller = ZoomableImageCropperController(
-          image: value.image,
-          aspectRatio: value.aspectRatio,
-          imageWidth: value.imageWidth,
-          imageHeight: value.imageHeight,
-          containerHeight: boxHeight,
-          onInteractionUpdate: (details) {
-            /* 
-              *  You can use the details to get the scale and translation values
-              *  to do something with it
-              */
-          },
-          containerWidth: size.width,
-        );
-        loadingImage = false;
-      });
-    });
-    super.initState();
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final XFile? picked = await picker.pickImage(source: ImageSource.gallery);
+    if (picked != null) {
+      setState(() => _image = File(picked.path));
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => ImageCropScreen(image: _image!)),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Zoomable cropper Demo'),
-      ),
-      body: SizedBox(
-        height: size.height,
-        child: Center(
-          child: loadingImage
-              ? const CircularProgressIndicator()
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      onPressed: changeAspectRatio,
-                      child: const Text('Change aspect ratio'),
-                    ),
-                    ZoomableImageCropper(controller: controller!),
-                  ],
-                ),
+      appBar: AppBar(title: const Text('Select Image')),
+      body: Center(
+        child: ElevatedButton.icon(
+          icon: const Icon(Icons.photo),
+          label: const Text('Pick from Gallery'),
+          onPressed: _pickImage,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final bytes = await controller!.getCurrentCropImageBytes();
-          // Use thes bytes to save the real file or send it to a server
-          print(bytes);
+    );
+  }
+}
+
+class ImageCropScreen extends StatefulWidget {
+  final File image;
+  const ImageCropScreen({super.key, required this.image});
+
+  @override
+  State<ImageCropScreen> createState() => _ImageCropScreenState();
+}
+
+class _ImageCropScreenState extends State<ImageCropScreen> {
+  bool _showCropOverlay = true;
+
+  final double handleSize = 20;
+  Rect cropRect = const Rect.fromLTWH(10, 10, 50, 50);
+
+  Offset? _dragStart;
+  Rect? _startRect;
+
+  void _onDragStart(DragStartDetails details) {
+    _dragStart = details.localPosition;
+    _startRect = cropRect;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    if (_dragStart == null || _startRect == null) return;
+    final dx = details.localPosition.dx - _dragStart!.dx;
+    final dy = details.localPosition.dy - _dragStart!.dy;
+    setState(() {
+      cropRect = _startRect!.translate(dx, dy);
+    });
+  }
+
+  void _onHandleDrag(DragUpdateDetails details, String corner) {
+    setState(() {
+      double newLeft = cropRect.left;
+      double newTop = cropRect.top;
+      double newRight = cropRect.right;
+      double newBottom = cropRect.bottom;
+
+      switch (corner) {
+        case 'tl':
+          newLeft += details.delta.dx;
+          newTop += details.delta.dy;
+          break;
+        case 'tr':
+          newRight += details.delta.dx;
+          newTop += details.delta.dy;
+          break;
+        case 'bl':
+          newLeft += details.delta.dx;
+          newBottom += details.delta.dy;
+          break;
+        case 'br':
+          newRight += details.delta.dx;
+          newBottom += details.delta.dy;
+          break;
+      }
+
+      cropRect = Rect.fromLTRB(newLeft, newTop, newRight, newBottom);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crop Image'),
+        actions: [
+          IconButton(
+            icon: Icon(_showCropOverlay ? Icons.zoom_out_map : Icons.crop),
+            onPressed: () {
+              setState(() => _showCropOverlay = !_showCropOverlay);
+            },
+          ),
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return InteractiveViewer(
+            boundaryMargin: const EdgeInsets.all(20),
+            minScale: 1.0,
+            maxScale: 4.0,
+            child: Stack(
+              children: [
+                Image.file(widget.image, fit: BoxFit.contain),
+                if (_showCropOverlay)
+                  Positioned.fill(
+                    child: GestureDetector(
+                      onPanStart: _onDragStart,
+                      onPanUpdate: _onDragUpdate,
+                      child: CustomPaint(painter: CropRectPainter(cropRect)),
+                    ),
+                  ),
+                if (_showCropOverlay) ..._buildHandles(),
+              ],
+            ),
+          );
         },
-        child: const Icon(Icons.save_alt_outlined),
       ),
     );
   }
 
-  Future<
-    ({FileImage image, double aspectRatio, int imageWidth, int imageHeight})
-  >
-  getPermissionAndSelectImage() async {
-    // I use photo manager to get the image from the gallery, but you can use any other method to get the image
-    final permitted = await PhotoManager.requestPermissionExtend();
-    if (!(permitted.isAuth || permitted.hasAccess)) {
-      await PhotoManager.openSetting();
-      getPermissionAndSelectImage();
-      // throw Exception('Permission not granted');
-    }
-    final albums = await PhotoManager.getAssetPathList();
-    final recentAlbum = albums.first;
-    final recentAlbumAssets = await recentAlbum.getAssetListRange(
-      start: 0,
-      end: 5,
-    );
-    final recentAsset = recentAlbumAssets.first;
-    final file = await recentAsset.file;
-
-    return (
-      image: FileImage(file!),
-      aspectRatio: recentAsset.width / recentAsset.height,
-      imageWidth: recentAsset.width,
-      imageHeight: recentAsset.height,
-    );
+  List<Widget> _buildHandles() {
+    return [
+      _handle(cropRect.topLeft, 'tl'),
+      _handle(cropRect.topRight, 'tr'),
+      _handle(cropRect.bottomLeft, 'bl'),
+      _handle(cropRect.bottomRight, 'br'),
+    ];
   }
 
-  changeAspectRatio() {
-    // For the demo, I just change the aspect ratio randomly
-    List<double> aspectRatios = [1.0, 16 / 12, 9 / 16];
-    double randomAspectRatio = aspectRatios[Random().nextInt(3)];
-    if (randomAspectRatio == controller?.aspectRatio) {
-      changeAspectRatio();
-      return;
-    }
-    setState(() {
-      controller?.setAspectRatio(randomAspectRatio);
-    });
+  Widget _handle(Offset offset, String corner) {
+    return Positioned(
+      left: offset.dx - handleSize / 2,
+      top: offset.dy - handleSize / 2,
+      child: GestureDetector(
+        onPanUpdate: (details) => _onHandleDrag(details, corner),
+        child: Container(
+          width: handleSize,
+          height: handleSize,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.black),
+            shape: BoxShape.circle,
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class CropRectPainter extends CustomPainter {
+  final Rect rect;
+  CropRectPainter(this.rect);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.white70
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2;
+
+    canvas.drawRect(rect, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
